733364a6d0db3c79e577b8a1b9fef929e30201e0
누리 김 <goldenworld036@gmail.com>
2023-10-19 22:42:19 +0900

    Goal -> Anm Btn

--- Calender/UpperLimit.Designer.cs
+++ Calender/UpperLimit.Designer.cs
@@ -72,25 +72,28 @@
             panel1.Controls.Add(btnSwitchTree);
             panel1.Controls.Add(btnSwitchCalender);
             panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(2, 2, 2, 2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(846, 95);
+            panel1.Size = new Size(658, 71);
             panel1.TabIndex = 1;
             // 
             // lbYearTop
             // 
             lbYearTop.AutoSize = true;
             lbYearTop.Font = new Font("Cascadia Mono", 30F, FontStyle.Bold, GraphicsUnit.Point);
-            lbYearTop.Location = new Point(699, 13);
+            lbYearTop.Location = new Point(544, 10);
+            lbYearTop.Margin = new Padding(2, 0, 2, 0);
             lbYearTop.Name = "lbYearTop";
-            lbYearTop.Size = new Size(144, 66);
+            lbYearTop.Size = new Size(114, 52);
             lbYearTop.TabIndex = 20;
             lbYearTop.Text = "2023";
             // 
             // btnSwitchUpper
             // 
-            btnSwitchUpper.Location = new Point(186, 13);
+            btnSwitchUpper.Location = new Point(145, 10);
+            btnSwitchUpper.Margin = new Padding(2, 2, 2, 2);
             btnSwitchUpper.Name = "btnSwitchUpper";
-            btnSwitchUpper.Size = new Size(49, 45);
+            btnSwitchUpper.Size = new Size(38, 34);
             btnSwitchUpper.TabIndex = 19;
             btnSwitchUpper.Text = "dd";
             btnSwitchUpper.UseVisualStyleBackColor = true;
@@ -98,20 +101,19 @@
             // btnSwitchGraph
             // 
             btnSwitchGraph.Image = (Image)resources.GetObject("btnSwitchGraph.Image");
-            btnSwitchGraph.Location = new Point(130, 13);
-            btnSwitchGraph.Margin = new Padding(4);
+            btnSwitchGraph.Location = new Point(101, 10);
             btnSwitchGraph.Name = "btnSwitchGraph";
-            btnSwitchGraph.Size = new Size(49, 45);
+            btnSwitchGraph.Size = new Size(38, 34);
             btnSwitchGraph.TabIndex = 17;
             btnSwitchGraph.UseVisualStyleBackColor = true;
+            btnSwitchGraph.Click += btnSwitchGraph_Click;
             // 
             // btnSwitchTree
             // 
             btnSwitchTree.Image = (Image)resources.GetObject("btnSwitchTree.Image");
-            btnSwitchTree.Location = new Point(73, 13);
-            btnSwitchTree.Margin = new Padding(4);
+            btnSwitchTree.Location = new Point(57, 10);
             btnSwitchTree.Name = "btnSwitchTree";
-            btnSwitchTree.Size = new Size(49, 45);
+            btnSwitchTree.Size = new Size(38, 34);
             btnSwitchTree.TabIndex = 18;
             btnSwitchTree.UseVisualStyleBackColor = true;
             btnSwitchTree.Click += btnSwitchTree_Click;
@@ -119,10 +121,9 @@
             // btnSwitchCalender
             // 
             btnSwitchCalender.Image = (Image)resources.GetObject("btnSwitchCalender.Image");
-            btnSwitchCalender.Location = new Point(16, 13);
-            btnSwitchCalender.Margin = new Padding(4);
+            btnSwitchCalender.Location = new Point(12, 10);
             btnSwitchCalender.Name = "btnSwitchCalender";
-            btnSwitchCalender.Size = new Size(49, 45);
+            btnSwitchCalender.Size = new Size(38, 34);
             btnSwitchCalender.TabIndex = 16;
             btnSwitchCalender.UseVisualStyleBackColor = true;
             btnSwitchCalender.Click += btnSwitchCalender_Click;
@@ -141,16 +142,18 @@
             pnlBottom.Controls.Add(lbText2);
             pnlBottom.Controls.Add(lbText1);
             pnlBottom.ForeColor = SystemColors.ControlText;
-            pnlBottom.Location = new Point(0, 367);
+            pnlBottom.Location = new Point(0, 275);
+            pnlBottom.Margin = new Padding(2, 2, 2, 2);
             pnlBottom.Name = "pnlBottom";
-            pnlBottom.Size = new Size(846, 134);
+            pnlBottom.Size = new Size(659, 102);
             pnlBottom.TabIndex = 2;
             // 
             // btnGiveUp
             // 
-            btnGiveUp.Location = new Point(716, 92);
+            btnGiveUp.Location = new Point(557, 69);
+            btnGiveUp.Margin = new Padding(2, 2, 2, 2);
             btnGiveUp.Name = "btnGiveUp";
-            btnGiveUp.Size = new Size(118, 29);
+            btnGiveUp.Size = new Size(92, 22);
             btnGiveUp.TabIndex = 10;
             btnGiveUp.Text = "챌린지 포기";
             btnGiveUp.UseVisualStyleBackColor = true;
@@ -159,44 +162,49 @@
             // lbText6
             // 
             lbText6.AutoSize = true;
-            lbText6.Location = new Point(383, 95);
+            lbText6.Location = new Point(298, 71);
+            lbText6.Margin = new Padding(2, 0, 2, 0);
             lbText6.Name = "lbText6";
-            lbText6.Size = new Size(173, 20);
+            lbText6.Size = new Size(143, 15);
             lbText6.TabIndex = 8;
             lbText6.Text = "금일 권장액 : 0000000원";
             // 
             // lbText5
             // 
             lbText5.AutoSize = true;
-            lbText5.Location = new Point(383, 60);
+            lbText5.Location = new Point(298, 45);
+            lbText5.Margin = new Padding(2, 0, 2, 0);
             lbText5.Name = "lbText5";
-            lbText5.Size = new Size(158, 20);
+            lbText5.Size = new Size(131, 15);
             lbText5.TabIndex = 7;
             lbText5.Text = "잔여 금액 : 0000000원";
             // 
             // lbText4
             // 
             lbText4.AutoSize = true;
-            lbText4.Location = new Point(383, 25);
+            lbText4.Location = new Point(298, 19);
+            lbText4.Margin = new Padding(2, 0, 2, 0);
             lbText4.Name = "lbText4";
-            lbText4.Size = new Size(158, 20);
+            lbText4.Size = new Size(131, 15);
             lbText4.TabIndex = 6;
             lbText4.Text = "초기 금액 : 0000000원";
             // 
             // label2
             // 
             label2.BorderStyle = BorderStyle.Fixed3D;
-            label2.Location = new Point(364, 13);
+            label2.Location = new Point(283, 10);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(3, 112);
+            label2.Size = new Size(2, 84);
             label2.TabIndex = 5;
             // 
             // lbD_Day
             // 
             lbD_Day.Font = new Font("맑은 고딕", 25F, FontStyle.Bold, GraphicsUnit.Point);
-            lbD_Day.Location = new Point(172, 50);
+            lbD_Day.Location = new Point(134, 38);
+            lbD_Day.Margin = new Padding(2, 0, 2, 0);
             lbD_Day.Name = "lbD_Day";
-            lbD_Day.Size = new Size(182, 71);
+            lbD_Day.Size = new Size(142, 53);
             lbD_Day.TabIndex = 4;
             lbD_Day.Text = "0일";
             lbD_Day.TextAlign = ContentAlignment.MiddleCenter;
@@ -204,44 +212,49 @@
             // lbText3
             // 
             lbText3.AutoSize = true;
-            lbText3.Location = new Point(219, 30);
+            lbText3.Location = new Point(170, 22);
+            lbText3.Margin = new Padding(2, 0, 2, 0);
             lbText3.Name = "lbText3";
-            lbText3.Size = new Size(84, 20);
+            lbText3.Size = new Size(67, 15);
             lbText3.TabIndex = 3;
             lbText3.Text = "종료일까지";
             // 
             // label1
             // 
             label1.BorderStyle = BorderStyle.Fixed3D;
-            label1.Location = new Point(163, 13);
+            label1.Location = new Point(127, 10);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(3, 112);
+            label1.Size = new Size(2, 84);
             label1.TabIndex = 2;
             // 
             // lbText2
             // 
             lbText2.AutoSize = true;
-            lbText2.Location = new Point(14, 82);
+            lbText2.Location = new Point(11, 62);
+            lbText2.Margin = new Padding(2, 0, 2, 0);
             lbText2.Name = "lbText2";
-            lbText2.Size = new Size(143, 20);
+            lbText2.Size = new Size(120, 15);
             lbText2.TabIndex = 1;
             lbText2.Text = "종료일 : 0000-00-00";
             // 
             // lbText1
             // 
             lbText1.AutoSize = true;
-            lbText1.Location = new Point(14, 33);
+            lbText1.Location = new Point(11, 25);
+            lbText1.Margin = new Padding(2, 0, 2, 0);
             lbText1.Name = "lbText1";
-            lbText1.Size = new Size(143, 20);
+            lbText1.Size = new Size(120, 15);
             lbText1.TabIndex = 0;
             lbText1.Text = "시작일 : 0000-00-00";
             // 
             // pnlCenter
             // 
             pnlCenter.Controls.Add(pnlNoChallenge);
-            pnlCenter.Location = new Point(0, 94);
+            pnlCenter.Location = new Point(0, 70);
+            pnlCenter.Margin = new Padding(2, 2, 2, 2);
             pnlCenter.Name = "pnlCenter";
-            pnlCenter.Size = new Size(846, 274);
+            pnlCenter.Size = new Size(658, 206);
             pnlCenter.TabIndex = 3;
             // 
             // pnlNoChallenge
@@ -257,79 +270,89 @@
             pnlNoChallenge.Controls.Add(btnStart);
             pnlNoChallenge.Controls.Add(lbNoChallenge);
             pnlNoChallenge.Location = new Point(0, 0);
+            pnlNoChallenge.Margin = new Padding(2, 2, 2, 2);
             pnlNoChallenge.Name = "pnlNoChallenge";
-            pnlNoChallenge.Size = new Size(846, 274);
+            pnlNoChallenge.Size = new Size(658, 206);
             pnlNoChallenge.TabIndex = 0;
             // 
             // lbMoney
             // 
             lbMoney.AutoSize = true;
-            lbMoney.Location = new Point(433, 168);
+            lbMoney.Location = new Point(337, 126);
+            lbMoney.Margin = new Padding(2, 0, 2, 0);
             lbMoney.Name = "lbMoney";
-            lbMoney.Size = new Size(164, 20);
+            lbMoney.Size = new Size(131, 15);
             lbMoney.TabIndex = 19;
             lbMoney.Text = "원으로 버텨보겠습니다";
             // 
             // lbDay
             // 
             lbDay.AutoSize = true;
-            lbDay.Location = new Point(531, 118);
+            lbDay.Location = new Point(413, 88);
+            lbDay.Margin = new Padding(2, 0, 2, 0);
             lbDay.Name = "lbDay";
-            lbDay.Size = new Size(59, 20);
+            lbDay.Size = new Size(47, 15);
             lbDay.TabIndex = 18;
             lbDay.Text = "일 까지";
             // 
             // lbMonth
             // 
             lbMonth.AutoSize = true;
-            lbMonth.Location = new Point(433, 118);
+            lbMonth.Location = new Point(337, 88);
+            lbMonth.Margin = new Padding(2, 0, 2, 0);
             lbMonth.Name = "lbMonth";
-            lbMonth.Size = new Size(24, 20);
+            lbMonth.Size = new Size(19, 15);
             lbMonth.TabIndex = 17;
             lbMonth.Text = "월";
             // 
             // lbYear
             // 
             lbYear.AutoSize = true;
-            lbYear.Location = new Point(345, 118);
+            lbYear.Location = new Point(268, 88);
+            lbYear.Margin = new Padding(2, 0, 2, 0);
             lbYear.Name = "lbYear";
-            lbYear.Size = new Size(24, 20);
+            lbYear.Size = new Size(19, 15);
             lbYear.TabIndex = 16;
             lbYear.Text = "년";
             // 
             // tbMoney
             // 
-            tbMoney.Location = new Point(241, 165);
+            tbMoney.Location = new Point(187, 124);
+            tbMoney.Margin = new Padding(2, 2, 2, 2);
             tbMoney.Name = "tbMoney";
-            tbMoney.Size = new Size(186, 27);
+            tbMoney.Size = new Size(146, 23);
             tbMoney.TabIndex = 15;
             // 
             // tbDay
             // 
-            tbDay.Location = new Point(469, 115);
+            tbDay.Location = new Point(365, 86);
+            tbDay.Margin = new Padding(2, 2, 2, 2);
             tbDay.Name = "tbDay";
-            tbDay.Size = new Size(54, 27);
+            tbDay.Size = new Size(43, 23);
             tbDay.TabIndex = 14;
             // 
             // tbMonth
             // 
-            tbMonth.Location = new Point(375, 115);
+            tbMonth.Location = new Point(292, 86);
+            tbMonth.Margin = new Padding(2, 2, 2, 2);
             tbMonth.Name = "tbMonth";
-            tbMonth.Size = new Size(52, 27);
+            tbMonth.Size = new Size(41, 23);
             tbMonth.TabIndex = 13;
             // 
             // tbYear
             // 
-            tbYear.Location = new Point(241, 115);
+            tbYear.Location = new Point(187, 86);
+            tbYear.Margin = new Padding(2, 2, 2, 2);
             tbYear.Name = "tbYear";
-            tbYear.Size = new Size(98, 27);
+            tbYear.Size = new Size(77, 23);
             tbYear.TabIndex = 12;
             // 
             // btnStart
             // 
-            btnStart.Location = new Point(363, 209);
+            btnStart.Location = new Point(282, 157);
+            btnStart.Margin = new Padding(2, 2, 2, 2);
             btnStart.Name = "btnStart";
-            btnStart.Size = new Size(94, 29);
+            btnStart.Size = new Size(73, 22);
             btnStart.TabIndex = 11;
             btnStart.Text = "도전하기";
             btnStart.UseVisualStyleBackColor = true;
@@ -339,22 +362,24 @@
             // 
             lbNoChallenge.AutoSize = true;
             lbNoChallenge.Font = new Font("맑은 고딕", 16F, FontStyle.Regular, GraphicsUnit.Point);
-            lbNoChallenge.Location = new Point(209, 36);
+            lbNoChallenge.Location = new Point(163, 27);
+            lbNoChallenge.Margin = new Padding(2, 0, 2, 0);
             lbNoChallenge.Name = "lbNoChallenge";
-            lbNoChallenge.Size = new Size(428, 37);
+            lbNoChallenge.Size = new Size(350, 30);
             lbNoChallenge.TabIndex = 10;
             lbNoChallenge.Text = "진행중인 지출 챌린지가 없습니다.";
             // 
             // UpperLimit
             // 
-            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonHighlight;
-            ClientSize = new Size(846, 501);
+            ClientSize = new Size(658, 376);
             Controls.Add(pnlCenter);
             Controls.Add(pnlBottom);
             Controls.Add(panel1);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2, 2, 2, 2);
             Name = "UpperLimit";
             Text = "UpperLimit";
             FormClosing += UpperLimit_FormClosing;
--- Calender/UpperLimit.cs
+++ Calender/UpperLimit.cs
@@ -485,5 +485,28 @@ namespace Ledger
                 panelToRemove.Dispose(); // 메모리 누수 방지를 위해 Dispose 호출
             }
         }
+
+        private void btnSwitchGraph_Click(object sender, EventArgs e)
+        {
+            foreach (Form openForm in Application.OpenForms)
+            {
+                // 폼 중복 열기 방지
+                if (openForm.Name == "Analysis") // 열린 폼의 이름 검사
+                {
+                    if (openForm.WindowState == FormWindowState.Minimized)
+                    {   // 폼이 active 인지 검사
+                        openForm.WindowState = FormWindowState.Normal;
+                        openForm.Location = new Point(this.Location.X, this.Location.Y);
+                    }
+                    openForm.Activate();
+                    openForm.Show();
+                    this.Hide();
+                    return;
+                }
+            }
+            Analysis Analysis = new Analysis(formMain); // 트리뷰 폼을 만들고 기존의 값들을 넘겨줌.
+            Analysis.Show();
+            this.Hide();
+        }
     }
 }
