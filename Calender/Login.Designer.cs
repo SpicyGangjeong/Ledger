4387882dbd3b6a85604185a3d661b387875652d3
pdplayer <pdplayer@naver.com>
2023-11-30 18:08:22 +0900

    로그인 폼에 초기값 지움

--- Calender/Login.Designer.cs
+++ Calender/Login.Designer.cs
@@ -1,7 +1,5 @@
-﻿namespace Ledger
-{
-    partial class Login
-    {
+﻿namespace Ledger {
+    partial class Login {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -11,10 +9,8 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -26,8 +22,7 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             panel1 = new Panel();
             panel2 = new Panel();
@@ -48,10 +43,9 @@
             panel1.BackColor = Color.Transparent;
             panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
             panel1.BackgroundImageLayout = ImageLayout.Stretch;
-            panel1.Location = new Point(146, 22);
-            panel1.Margin = new Padding(2, 2, 2, 2);
+            panel1.Location = new Point(188, 29);
             panel1.Name = "panel1";
-            panel1.Size = new Size(98, 93);
+            panel1.Size = new Size(126, 124);
             panel1.TabIndex = 0;
             // 
             // panel2
@@ -59,10 +53,9 @@
             panel2.BackColor = Color.Transparent;
             panel2.BackgroundImage = (Image)resources.GetObject("panel2.BackgroundImage");
             panel2.BackgroundImageLayout = ImageLayout.Stretch;
-            panel2.Location = new Point(53, 127);
-            panel2.Margin = new Padding(2, 2, 2, 2);
+            panel2.Location = new Point(68, 169);
             panel2.Name = "panel2";
-            panel2.Size = new Size(285, 37);
+            panel2.Size = new Size(366, 49);
             panel2.TabIndex = 1;
             // 
             // label1
@@ -70,10 +63,9 @@
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(170, 176);
-            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Location = new Point(219, 235);
             label1.Name = "label1";
-            label1.Size = new Size(58, 21);
+            label1.Size = new Size(72, 28);
             label1.TabIndex = 2;
             label1.Text = "로그인";
             // 
@@ -87,20 +79,18 @@
             panel3.Controls.Add(label2);
             panel3.Controls.Add(userPw);
             panel3.Controls.Add(userId);
-            panel3.Location = new Point(33, 208);
-            panel3.Margin = new Padding(2, 2, 2, 2);
+            panel3.Location = new Point(42, 277);
             panel3.Name = "panel3";
-            panel3.Size = new Size(324, 150);
+            panel3.Size = new Size(417, 200);
             panel3.TabIndex = 3;
             // 
             // btnSearchPw
             // 
             btnSearchPw.BackColor = Color.WhiteSmoke;
             btnSearchPw.FlatStyle = FlatStyle.Flat;
-            btnSearchPw.Location = new Point(162, 98);
-            btnSearchPw.Margin = new Padding(2, 2, 2, 2);
+            btnSearchPw.Location = new Point(208, 131);
             btnSearchPw.Name = "btnSearchPw";
-            btnSearchPw.Size = new Size(149, 28);
+            btnSearchPw.Size = new Size(192, 37);
             btnSearchPw.TabIndex = 6;
             btnSearchPw.Text = "아이디/비밀번호 찾기";
             btnSearchPw.UseVisualStyleBackColor = false;
@@ -110,10 +100,9 @@
             // 
             btnSignUp.BackColor = Color.WhiteSmoke;
             btnSignUp.FlatStyle = FlatStyle.Flat;
-            btnSignUp.Location = new Point(12, 98);
-            btnSignUp.Margin = new Padding(2, 2, 2, 2);
+            btnSignUp.Location = new Point(15, 131);
             btnSignUp.Name = "btnSignUp";
-            btnSignUp.Size = new Size(145, 28);
+            btnSignUp.Size = new Size(186, 37);
             btnSignUp.TabIndex = 5;
             btnSignUp.Text = "회원가입";
             btnSignUp.UseVisualStyleBackColor = false;
@@ -123,10 +112,9 @@
             // 
             btnLogin.BackColor = Color.WhiteSmoke;
             btnLogin.FlatStyle = FlatStyle.Flat;
-            btnLogin.Location = new Point(226, 22);
-            btnLogin.Margin = new Padding(2, 2, 2, 2);
+            btnLogin.Location = new Point(291, 29);
             btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(73, 57);
+            btnLogin.Size = new Size(94, 76);
             btnLogin.TabIndex = 4;
             btnLogin.Text = "로그인";
             btnLogin.UseVisualStyleBackColor = false;
@@ -135,60 +123,53 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(15, 62);
-            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Location = new Point(19, 83);
             label3.Name = "label3";
-            label3.Size = new Size(55, 15);
+            label3.Size = new Size(69, 20);
             label3.TabIndex = 3;
             label3.Text = "비밀번호";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(26, 25);
-            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Location = new Point(33, 33);
             label2.Name = "label2";
-            label2.Size = new Size(43, 15);
+            label2.Size = new Size(54, 20);
             label2.TabIndex = 2;
             label2.Text = "아이디";
             // 
             // userPw
             // 
             userPw.BackColor = Color.WhiteSmoke;
-            userPw.Location = new Point(73, 59);
-            userPw.Margin = new Padding(2, 2, 2, 2);
+            userPw.Location = new Point(94, 79);
             userPw.Name = "userPw";
             userPw.PasswordChar = '*';
-            userPw.Size = new Size(142, 23);
+            userPw.Size = new Size(181, 27);
             userPw.TabIndex = 1;
-            userPw.Text = "test_pw";
             userPw.KeyDown += userPw_KeyDown;
             // 
             // userId
             // 
             userId.BackColor = Color.WhiteSmoke;
-            userId.Location = new Point(73, 22);
-            userId.Margin = new Padding(2, 2, 2, 2);
+            userId.Location = new Point(94, 29);
             userId.Name = "userId";
-            userId.Size = new Size(142, 23);
+            userId.Size = new Size(181, 27);
             userId.TabIndex = 0;
-            userId.Text = "test_id";
             userId.KeyDown += userId_KeyDown;
             // 
             // Login
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(390, 388);
+            ClientSize = new Size(501, 517);
             Controls.Add(panel3);
             Controls.Add(label1);
             Controls.Add(panel2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(2, 2, 2, 2);
             Name = "Login";
             Text = "로그인";
             panel3.ResumeLayout(false);
--- Calender/Login.cs
+++ Calender/Login.cs
@@ -9,10 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Ledger
-{
-    public partial class Login : Form
-    {
+namespace Ledger {
+    public partial class Login : Form {
         // 데이터베이스 연동
         public static string strConn = "Server=ledgerdb.ctsyekhyqkwe.ap-northeast-2.rds.amazonaws.com;Port=3306;Database=ledgerdb;Uid=root;Pwd=rootpass";
         public static MySqlConnection conn = null;
@@ -22,8 +20,7 @@ namespace Ledger
         public static String logined_user = "";
         public static String logined_id = "";
 
-        public Login()
-        {
+        public Login() {
 
             conn = new MySqlConnection(strConn);
             conn.Open();
@@ -31,29 +28,25 @@ namespace Ledger
             InitializeComponent();
         }
 
-        private void btnSignUp_Click(object sender, EventArgs e)
-        {
+        private void btnSignUp_Click(object sender, EventArgs e) {
             // 회원가입 폼 열기
             SignUp signup = new SignUp(this);
             signup.ShowDialog();
         }
 
-        private void btnSearchPw_Click(object sender, EventArgs e)
-        {
+        private void btnSearchPw_Click(object sender, EventArgs e) {
             // 아이디 / 비밀번호 찾기 폼 열기
             SeekAccount signup = new SeekAccount(this);
             signup.ShowDialog();
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
-        {
+        private void btnLogin_Click(object sender, EventArgs e) {
             String sql = "select * from tb_user where f_id='" + userId.Text
                 + "' and f_pw='" + userPw.Text + "'";
             MySqlCommand cmd = new MySqlCommand(sql, conn);
             MySqlDataReader data = cmd.ExecuteReader();
 
-            if (data.Read())
-            {
+            if (data.Read()) {
                 // 로그인 정보 일치시, 로그인 정보에 해당 로그인 정보를 저장
                 islogined = true;
                 logined_user = data["f_name"].ToString();
@@ -63,14 +56,13 @@ namespace Ledger
                 userId.Text = "";
                 userPw.Text = "";
 
-                
+
 
                 FormMain formMain = new FormMain(this);
                 formMain.Show();
                 this.Hide();
             }
-            else
-            {
+            else {
                 // 로그인 정보 불일치 시, 로그인 정보를 다시 입력할 수 있게 함
                 MessageBox.Show("아이디가 존재하지 않거나 비밀번호가 다릅니다.", "경고!",
                     MessageBoxButtons.OK, MessageBoxIcon.Warning);
@@ -82,18 +74,14 @@ namespace Ledger
         }
 
         // 엔터키로 로그인을 할 수 있게끔 설정
-        private void userPw_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
+        private void userPw_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
                 btnLogin_Click(sender, e);
             }
         }
 
-        private void userId_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
+        private void userId_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
                 btnLogin_Click(sender, e);
             }
         }
